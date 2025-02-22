0918cc43af00792255ea1cda52f8fddc552b627c
Craig West <iwswordpress@gmail.com>
2025-02-22 02:41:03 +0000

    test

--- 20_planning_agent_w_loop.py
+++ 20_planning_agent_w_loop.py
@@ -12,7 +12,7 @@ load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 if openai_api_key:
-    print(f"OpenAI API Key exists and begins:{openai_api_key[:10]}")
+    print(f"OpenAI API Key exists and begins:{openai_api_key[:14]}...")
 else:
     print("OpenAI API Key not set")
 
